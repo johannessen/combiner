46be8b7b55ac422c1876363f37cb6d2a029e0f6f
Arne Johannessen <git+3147@arne.johannessen.de>
2013-03-08 17:51:44 +0100

    OSM highway keys now sortable by class.
    The key values are simply normal Strings, so in order to be easily sortable using the normal Java Compareables, they need to be rewritten to other string values, the natural ordering of which conforms to the ordering implied by the OSM highway classes.

--- project/src/de/thaw/thesis/comb/io/ShapeTagsAdapter.java
+++ project/src/de/thaw/thesis/comb/io/ShapeTagsAdapter.java
@@ -47,16 +47,48 @@ final class ShapeTagsAdapter implements OsmTags {
 				return "no";
 			}
 		}
+		if (osmKey == "highway") {
+			if (osmKey == "primary") {
+				return "x3_primary";
+			}
+			if (osmKey == "primary_link") {
+				return "x3_primary_link";
+			}
+			if (osmKey == "secondary") {
+				return "x4_secondary";
+			}
+			if (osmKey == "secondary_link") {
+				return "x4_secondary_link";
+			}
+			if (osmKey == "tertiary") {
+				return "x5_tertiary";
+			}
+			if (osmKey == "tertiary_link") {
+				return "x5_tertiary_link";
+			}
+			if (osmKey == "unclassified") {
+				return "x6_unclassified";
+			}
+			if (osmKey == "unclassified_link") {
+				return "x6_unclassified_link";
+			}
+			if (osmKey == "residential") {
+				return "x7_residential";
+			}
+			if (osmKey == "residential_link") {
+				return "x7_residential_link";
+			}
+		}
 		return value;
 	}
 	
 	
 	public String get (final String key) {
-		Object attrValue = feature.getAttribute(rewriteKey(key));
+		Object attrValue = feature.getAttribute(rewriteKey(key.intern()));
 		if (attrValue == null) {
 			return OsmTags.NO_VALUE;
 		}
-		String stringValue = rewriteValue(key, attrValue.toString());
+		String stringValue = rewriteValue(key, attrValue.toString().intern());
 		return stringValue.intern();
 	}
 	
