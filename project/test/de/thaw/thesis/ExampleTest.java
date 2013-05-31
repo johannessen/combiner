cdea201ed9b2a401b2652e3a2295c7a16b59e5c4
Arne Johannessen <git+3147@arne.johannessen.de>
2013-05-31 21:30:58 +0200

    set up unit test environment (TestNG)

---
+++ project/test/de/thaw/thesis/ExampleTest.java
@@ -0,0 +1,17 @@
+package de.thaw.thesis;
+ 
+import org.testng.annotations.*;
+ 
+public class ExampleTest {
+ 
+ @BeforeClass
+ public void setUp() {
+   // code that will be invoked when this test is instantiated
+ }
+ 
+ @Test
+ public void aTest() {
+   System.out.println("the test");
+ }
+ 
+}
