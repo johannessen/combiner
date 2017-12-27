e1aaca93d042a07726fa5541b51cbea302778964
Arne Johannessen <git@arne.johannessen.de>
2017-12-27 16:01:55 +0100

    More readable runtime classpath

--- project/run-combiner.sh
+++ project/run-combiner.sh
@@ -31,8 +31,26 @@ OUT=../data/combiner/out.json
 # <http://docs.geotools.org/stable/userguide/faq.html#q-what-jars-does-gt-referencing-need>
 # <http://article.gmane.org/gmane.comp.gis.geotools2.user/15863>
 
+CLASSPATH=`perl -e 'print join ":", qw(
+	build/classes
+	lib/*
+	lib/geotools-18.0/core-0.26.jar
+	lib/geotools-18.0/gt-api-18.0.jar
+	lib/geotools-18.0/gt-data-18.0.jar
+	lib/geotools-18.0/gt-epsg-hsql-18.0.jar
+	lib/geotools-18.0/gt-main-18.0.jar
+	lib/geotools-18.0/gt-metadata-18.0.jar
+	lib/geotools-18.0/gt-opengis-18.0.jar
+	lib/geotools-18.0/gt-referencing-18.0.jar
+	lib/geotools-18.0/gt-shapefile-18.0.jar
+	lib/geotools-18.0/hsqldb-2.3.0.jar
+	lib/geotools-18.0/jsr-275-1.0-beta-2.jar
+	lib/geotools-18.0/jts-core-1.14.0.jar
+	lib/geotools-18.0/sqlite-jdbc-3.20.0.jar
+)'`
+
 time java \
-		-cp build/classes:lib/*:lib/geotools-18.0/core-0.26.jar:lib/geotools-18.0/gt-api-18.0.jar:lib/geotools-18.0/gt-data-18.0.jar:lib/geotools-18.0/gt-epsg-hsql-18.0.jar:lib/geotools-18.0/gt-main-18.0.jar:lib/geotools-18.0/gt-metadata-18.0.jar:lib/geotools-18.0/gt-opengis-18.0.jar:lib/geotools-18.0/gt-referencing-18.0.jar:lib/geotools-18.0/gt-shapefile-18.0.jar:lib/geotools-18.0/hsqldb-2.3.0.jar:lib/geotools-18.0/jsr-275-1.0-beta-2.jar:lib/geotools-18.0/jts-core-1.14.0.jar:lib/geotools-18.0/sqlite-jdbc-3.20.0.jar \
+		-cp $CLASSPATH \
 		-ea:de.thaw... \
 		-Djava.awt.headless=true \
 		-verbose:gc -XX:+PrintGCDetails -XX:+PrintGCTimeStamps \
